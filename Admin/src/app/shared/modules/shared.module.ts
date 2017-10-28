11254f25ba70632088c819b9a4458cb41de7eef1
Adam Dłubak <adam.dlubak@gmail.com>
2017-10-28 11:33:30 +0200

    Duplicate Client as a new Admin App

---
+++ Admin/src/app/shared/modules/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+import { myFocus } from "../../directives/focus.directive";
+import { SpinnerComponent } from "../../components/shared/spinner/spinner.component";
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [myFocus, SpinnerComponent],
+  exports: [myFocus, SpinnerComponent],
+  providers: []
+})
+export class SharedModule {}
---
+++ Client/src/app/shared/models/user.edit.interface.ts
@@ -0,0 +1,8 @@
+export interface UserEdit {
+    email: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    deliveryPostCode: string;
+  }
+  
